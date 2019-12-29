f1d08f20b97086958dfdac50e414bcfe6302a048
Rams <Rams@DESKTOP-7DUCIV9>
2019-12-30 00:24:24 +0530

    Initial Commit

---
+++ MLAIPlatform/src/main/java/org/mlaiplatform/application/MLAIApplication.java
@@ -0,0 +1,16 @@
+package org.mlaiplatform.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLAIApplication {
+	public static void main(String[] args) throws Exception {
+		System.out.println(" ***********  This is Spring Boot Application ************ ");
+		SpringApplication.run(MLAIApplication.class, args);
+	}
+
+	
+	
+
+}
