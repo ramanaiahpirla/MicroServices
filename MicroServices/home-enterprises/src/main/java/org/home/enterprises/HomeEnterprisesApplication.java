c6897f55285cedb7b3d0f56ed56ee7710e32e235
RP00490596 <RP00490596@TechMahindra.com>
2021-06-19 19:13:51 +0530

    updated code

---
+++ MicroServices/home-enterprises/src/main/java/org/home/enterprises/HomeEnterprisesApplication.java
@@ -0,0 +1,13 @@
+package org.home.enterprises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEnterprisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeEnterprisesApplication.class, args);
+	}
+
+}
